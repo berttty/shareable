9bcf3f3e828dedab7d266b8ffc11886b98a812ec
Bertty Contreras-Rojas <berttycontreras@gmail.com>
2022-11-18 00:15:15 +0200

    base for AnswerView v
    Signed-off-by: Bertty Contreras-Rojas <berttycontreras@gmail.com>

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,21 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import AnswerView from "../views/AnswerView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  linkActiveClass: 'active',
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "answer",
+      component: AnswerView,
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/memories",
+      name: "memories",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("../views/MemoriesView.vue"),
     },
   ],
 });
---
+++ web/src/views/AnswerView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import {getRandomInt} from '../utils/random'
+
+export interface Props {
+  question?: string
+  counter?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  question: () => {
+    //TODO recovery from database
+    let queries: string[] = [
+      'query 1: Lorem ipsum dolor sit amet, consectetur adipiscing?',
+      'query 2: Lorem ipsum dolor sit amet, consectetur adipiscing?',
+      'query 3: Lorem ipsum dolor sit amet, consectetur adipiscing?',
+      'query 4: Lorem ipsum dolor sit amet, consectetur adipiscing?',
+      'query 5: Lorem ipsum dolor sit amet, consectetur adipiscing?',
+      'query 6: Lorem ipsum dolor sit amet, consectetur adipiscing?',
+      'query 7: Lorem ipsum dolor sit amet, consectetur adipiscing?',
+      'query 8: Lorem ipsum dolor sit amet, consectetur adipiscing?',
+      'query 9: Lorem ipsum dolor sit amet, consectetur adipiscing?',
+      'query 0: Lorem ipsum dolor sit amet, consectetur adipiscing?'];
+    return queries[getRandomInt(queries.length)];
+  },
+  counter: 1000
+})
+</script>
+
+<template>
+  <div class="p-2">
+    <h1>{{question}}</h1>
+    <form class="mt-2">
+      <div class="form-check ps-0">
+        <label for="exampleInputPassword1" class="form-label">Write your mind you have {{ counter }} characters</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+      </div>
+      <div class="d-grid gap-2 pt-2">
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
