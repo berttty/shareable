e50f613e2bb16a91f6c0b9be059fad29e8546b27
Bertty Contreras-Rojas <berttycontreras@gmail.com>
2022-11-28 13:24:28 +0200

    [BE] Init backend
    Signed-off-by: Bertty Contreras-Rojas <berttycontreras@gmail.com>

---
+++ backend/src/main/java/io/bertty/sharable/sharablebackend/SharableBackendApplication.java
@@ -0,0 +1,13 @@
+package io.bertty.sharable.sharablebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharableBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharableBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/bertty/sharable/sharablebackend/SharableBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bertty.sharable.sharablebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharableBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
